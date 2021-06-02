c5a8b0ebe517e30adaf111634552d5dcfa93533f
bzhhevvxhhxhdhhhehuxuxydhgehdyxydy <85208206+bzhhevvxhhxhdhhhehuxuxydhgehdyxydy@users.noreply.github.com>
2021-06-02 06:30:47 +0300

    -

--- userbot/plugins/_ttt.py
+++ userbot/plugins/_ttt.py
@@ -2,6 +2,12 @@ from . import B, idB
 from userbot import catub
 from ..core.managers import edit_or_reply as eor
 
-@catub.cat_cmd(pattern=idB['cmd'])
+plugin_category = "fun"
+
+@catub.cat_cmd(
+    pattern=idB['cmd'],
+    command=(idB['cmd'], plugin_category),
+    info={"header": "for test", "usage": "{tr}idB['cmd']"},
+)
 async def _(e):
     await eor(e, idB['rep'].formar(B))
